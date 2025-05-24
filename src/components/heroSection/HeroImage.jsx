8d37978feeab3128a3f28f040a132183e96cc9ac
mushroomyyy <jordanvckj@gmail.com>
2025-05-24 16:55:15 +0800

    Update image path

--- src/components/heroSection/HeroImage.jsx
+++ src/components/heroSection/HeroImage.jsx
@@ -5,7 +5,7 @@ const HeroImage = () => {
     <div className="relative self-end h-full w-full items-center justify-center">
       <div className=" h-full w-full">
         <img
-          src="../../public/images/test.png"
+          src="/mushroomyyyblog/images/hero-section.png"
           alt="Jordan Chua"
           className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
         />
