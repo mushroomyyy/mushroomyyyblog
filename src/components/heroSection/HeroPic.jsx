2778c08d52e6c46027c292c998a55fc028346ae8
mushroomyyy <jordanvckj@gmail.com>
2025-05-24 17:15:13 +0800

    Update image path

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/test.png"
+        src="/mushroomyyyblog/images/test.png"
         alt="Jordan Chua"
         className="max-h-[450px] w-auto"
       />
